32a5f2deccd2248a4a294387aa0b61b7167aea5a
kim <altroman@yeah.net>
2025-06-24 21:46:49 +0800

    EXERCISE_6_4

--- src/Chapter 6 Drawing in Direct3D/Box/BoxApp.cpp
+++ src/Chapter 6 Drawing in Direct3D/Box/BoxApp.cpp
@@ -75,7 +75,7 @@ private:
 
     float mTheta = 1.5f*XM_PI;
     float mPhi = XM_PIDIV4;
-    float mRadius = 5.0f;
+    float mRadius = 10.0f;
 
     POINT mLastMousePos;
 };
@@ -365,41 +365,21 @@ void BoxApp::BuildBoxGeometry()
 {
     std::array<Vertex, 8> vertices =
     {
-        Vertex({ XMFLOAT3(-1.0f, -1.0f, -1.0f), XMFLOAT4(Colors::White) }),
-		Vertex({ XMFLOAT3(-1.0f, +1.0f, -1.0f), XMFLOAT4(Colors::Black) }),
-		Vertex({ XMFLOAT3(+1.0f, +1.0f, -1.0f), XMFLOAT4(Colors::Red) }),
-		Vertex({ XMFLOAT3(+1.0f, -1.0f, -1.0f), XMFLOAT4(Colors::Green) }),
-		Vertex({ XMFLOAT3(-1.0f, -1.0f, +1.0f), XMFLOAT4(Colors::Blue) }),
-		Vertex({ XMFLOAT3(-1.0f, +1.0f, +1.0f), XMFLOAT4(Colors::Yellow) }),
-		Vertex({ XMFLOAT3(+1.0f, +1.0f, +1.0f), XMFLOAT4(Colors::Cyan) }),
-		Vertex({ XMFLOAT3(+1.0f, -1.0f, +1.0f), XMFLOAT4(Colors::Magenta) })
+		Vertex({ XMFLOAT3(0.0f, +1.0f,   0.0f), XMFLOAT4(Colors::Red) }), //0
+		Vertex({ XMFLOAT3(-1.0f, -1.0f, -1.0f), XMFLOAT4(Colors::Green) }), //1
+		Vertex({ XMFLOAT3(1.0f, -1.0f,  -1.0f), XMFLOAT4(Colors::Green) }), //2
+		Vertex({ XMFLOAT3(+1.0f, -1.0f,  1.0f), XMFLOAT4(Colors::Green) }), //3
+		Vertex({ XMFLOAT3(-1.0f,  -1.0f,  1.0f), XMFLOAT4(Colors::Green)   }), //4
     };
 
 	std::array<std::uint16_t, 36> indices =
 	{
-		// front face
-		0, 1, 2,
-		0, 2, 3,
-
-		// back face
-		4, 6, 5,
-		4, 7, 6,
-
-		// left face
-		4, 5, 1,
-		4, 1, 0,
-
-		// right face
-		3, 2, 6,
-		3, 6, 7,
-
-		// top face
-		1, 5, 6,
-		1, 6, 2,
-
-		// bottom face
-		4, 0, 3,
-		4, 3, 7
+        0, 1, 2,
+        0, 2, 3,
+        0, 3, 4,
+        0, 4, 1,
+        1, 3, 2,
+        1, 4, 3
 	};
 
     const UINT vbByteSize = (UINT)vertices.size() * sizeof(Vertex);
